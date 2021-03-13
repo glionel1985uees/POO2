9c22f9340804d1b0d7443ec2da9904dc92f65869
gerson <gersonleoneln@hotmail.com>
2021-03-13 09:16:06 -0600

    Prueba de git.

---
+++ src/main/java/Guia2/ArrayListExam2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Guia2;
+
+/**
+ *
+ * @author GERSON
+ */
+public class ArrayListExam2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Crearas los siguientes objetos ArrayList que contengan:
+        //- Los meses del año
+        //- Marcas de vehículos
+        //- Marcas de ropa
+        //- Pasatiempos favoritos
+    }
+    
+}
---
+++ src/main/java/Guia2/HashMapExam2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Guia2;
+
+/**
+ *
+ * @author GERSON
+ */
+public class HashMapExam2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+        // Crea dos objetos HashMap el cual contenga veinte posiciones de tipo <Integer,String> queda bajo el criterio 
+        //del estudiante lo que contendrá cada posición y el nombre de la clase de tipo “Main” será HashMapExam2.
+    }
+    
+}
